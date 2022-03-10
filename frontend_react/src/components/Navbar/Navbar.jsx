a52f1c0d1fdf3bca2a4fc996aeb2e4acaba3a479
Vladimir Aleksic <vlada.pzs@hotmail.com>
2022-03-10 20:53:19 +0100

    navbar + css

---
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion } from 'framer-motion'
+
+import { images } from '../../constants'
+import './Navbar.scss'
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleOnClick = (e) => {
+    e.stopPropagation();
+    setToggle((prevState) => !prevState);
+  }
+
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt='logo' />
+
+        <div className='app__navbar-company-info'>
+          <p className='p-text'>Bukovacki put 75,</p>
+          <p className='p-text'>21000 Novi Sad</p>
+          <p className='p-text'>Tel: 021/111-111</p>
+        </div>
+      </div>
+
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'materials', 'contact'].map((item) => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={(e) => handleOnClick(e)} />
+
+        {toggle && (
+          <motion.div
+            initial={{ width: 0 }}
+            animate={{ width: 300 }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <motion.span
+              initial={{ width: 0 }}
+              animate={{ width: 70 }}
+              transition={{ duration: 0.85, ease: 'easeOut' }}
+            >
+              <HiX onClick={() => setToggle(false)} />
+            </motion.span>
+            <ul>
+              {['home', 'about', 'work', 'materials', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
